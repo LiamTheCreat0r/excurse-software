0491f62508898f172c7f6ed98d74206319643175
Liam <106878018+LiamTheCreat0r@users.noreply.github.com>
2026-05-11 10:20:33 +0200

    feat: github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://LiamTheCreat0r.github.io',
+  base: '/excurse-software',
+});
